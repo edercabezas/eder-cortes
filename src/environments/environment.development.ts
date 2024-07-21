1ba219d2ee7f1fb2672f3f824f3e2f0058fb0fa8
Eder Arley Cortes <edercortez10@gmail.com>
2024-07-20 19:33:05 -0500

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDXG7-0XBUBwn4V6DmmUjMEm68fcgY26aQ",
+    authDomain: "generacion-fuego.firebaseapp.com",
+    projectId: "generacion-fuego",
+    storageBucket: "generacion-fuego.appspot.com",
+    messagingSenderId: "290017553740",
+    appId: "1:290017553740:web:dd46b8dbf8fed0f89e33e9",
+    measurementId: "G-QDJYRNXGCL"
+  }
+};
